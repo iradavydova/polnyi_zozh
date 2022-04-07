03f23cfa3e067bdf1f158070e50acc93c8205fac
Alena Nemtsova <jelenatbs@gmail.com>
2022-04-07 21:22:30 +0300

    add js file

---
+++ assets/js/progressbar.js
@@ -0,0 +1,40 @@
+/* let myBar = document.getElementById('my-progress-bar');
+let laberBar = document.getElementById('progress-label');
+let chbox = document.getElementsByClassName('select-done');
+
+let increaseProgress = function increase() {
+    myBar.value += 1;
+    myBar.innerHTML = myBar.value + '%';
+    if(myBar.value < 7) {
+        laberBar.innerHTML = myBar.value + '%';
+    }
+    laberBar.innerHTML = "100%"
+
+} */
+
+let laberBar = document.getElementById('progress-label');
+let myBar = document.getElementById("my-progress-bar");
+let markDone = document.getElementById("mark-done");
+
+function increase() {
+    let checked = 0;
+    
+    boxes = markDone.querySelectorAll("input[type='checkbox']:checked");
+    checked = boxes.length;
+
+    if (myBar.value < 7) {
+        laberBar.innerHTML = myBar.value + '%';
+        myBar.value += 1;
+    }
+    laberBar.innerHTML = "100%";
+
+    myBar.value = ((checked / 7) * 100) + "%";
+
+    }
+    
+    checks = document.querySelectorAll("input[type='checkbox']");
+    checks.forEach(function(box) {
+    box.addEventListener("change", function(e) {
+        increase()
+    });
+});
