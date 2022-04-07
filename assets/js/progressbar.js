21e7e294c444d8997aef44dfccae28b0deccc3aa
Maria2721 <84140203+Maria2721@users.noreply.github.com>
2022-04-07 22:05:16 +0300

    add checked in LS

--- assets/js/progressbar.js
+++ assets/js/progressbar.js
@@ -15,10 +15,12 @@ let increaseProgress = function increase() {
 let laberBar = document.getElementById('progress-label');
 let myBar = document.getElementById("my-progress-bar");
 let markDone = document.getElementById("mark-done");
+console.log(markDone)
 
 function increase() {
     let checked = 0;
-    
+    myBar.value = 0;
+
     boxes = markDone.querySelectorAll("input[type='checkbox']:checked");
     checked = boxes.length;
 
@@ -29,12 +31,12 @@ function increase() {
     laberBar.innerHTML = "100%";
 
     myBar.value = ((checked / 7) * 100) + "%";
+}
 
-    }
-    
-    checks = document.querySelectorAll("input[type='checkbox']");
-    checks.forEach(function(box) {
-    box.addEventListener("change", function(e) {
-        increase()
+checks = document.querySelectorAll("input[type='checkbox']");
+checks.forEach(function (box) {
+    box.addEventListener("change", function (e) {
+        //increase();
+        addContent();
     });
 });
