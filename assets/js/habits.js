44111ae8cd0852f6ecb050fe1618b8964d221870
Maria2721 <84140203+Maria2721@users.noreply.github.com>
2022-04-08 01:35:57 +0300

    add checkbox content

--- assets/js/habits.js
+++ assets/js/habits.js
@@ -4,13 +4,23 @@ class Note {
         this.text = text;
     }
 }
+class Daycheck {
+    constructor(monday, tuesday, wednesday, thursday, friday, saturday, sunday) {
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
+        this.saturday = saturday;
+        this.sunday = sunday;
+    }
+}
 
-const list = document.querySelector('.list');
 const addBtn = document.querySelector(".addBtn");
 const clearBtn = document.querySelector('.clearBtn');
 const noteInput = document.querySelector('.note');
 let itemsArray = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
-let notesCount = localStorage.getItem('notesCount') ? JSON.parse(localStorage.getItem('notesCount')) : [];
+let notesCount = localStorage.getItem('notesCount') ? JSON.parse(localStorage.getItem('notesCount')) : 0;
 
 itemsArray.forEach(item => {
     liMaker(item);
@@ -21,58 +31,104 @@ function addStuff() {
     if (noteText.length === 0) return;
 
     localStorage.setItem('notesCount', notesCount);
-    let note = new Note(+notesCount, noteText);
-    notesCount += +1;
-    localStorage.setItem('notesCount', notesCount);
+    let note = new Note(notesCount, noteText);
     itemsArray.push(note);
     localStorage.setItem('notes', JSON.stringify(itemsArray));
+    let weekcheck = new Daycheck(false, false, false, false, false, false, false);
+    localStorage.setItem(`${note.id}`, JSON.stringify(weekcheck));
+    notesCount += 1;
+    localStorage.setItem('notesCount', notesCount);
 
     liMaker(note);
-
     noteInput.value = "";
 }
 
 function liMaker(note) {
-    const btn = document.createElement('button');
-    btn.type = 'button';
-    btn.className = 'deleteBtn';
+    const btn_content = document.createElement('button');
+    btn_content.type = 'button';
+    btn_content.className = 'contentBtn';
+    btn_content.textContent = `${note.text}`;
+    btn_content.addEventListener("click", (e) => addContent(e));
+
+    const btn_delete = document.createElement('button');
+    btn_delete.type = 'button';
+    btn_delete.className = 'deleteBtn';
     const img = document.createElement('img');
     img.setAttribute('src', '/assets/images/cross.svg');
-    btn.appendChild(img);
-    btn.addEventListener("click", (e) => deleteNote(e));
+    btn_delete.appendChild(img);
+    btn_delete.addEventListener("click", (e) => deleteNote(e));
 
-    const li = document.createElement('li');
-    li.textContent = `${note.text} `;
-    li.id = 'mynote';
-    li.value = note.id;
-    li.setAttribute("onclick", "addContent()");
-    li.appendChild(btn);
-    console.log(li)
-    list.appendChild(li);
+    const div_note = document.createElement('div');
+    div_note.id = `${note.id}`;
+    div_note.value = note.id;
+    div_note.appendChild(btn_content);
+    div_note.appendChild(btn_delete);
+    document.getElementById('list_habit').appendChild(div_note);
 }
 
 function deleteNote(e) {
-    let parent = e.target.parentElement.closest('li');
-    console.log(parent)
+    let parent = e.target.parentElement.closest('div');
     parent.remove();
 
     localStorage.removeItem('notes');
     filteredItemsArray = itemsArray.filter((item) => item.id != parent.value);
     localStorage.setItem('notes', JSON.stringify(filteredItemsArray));
+    localStorage.removeItem(`${parent.value}`);
     itemsArray = filteredItemsArray;
 }
 
 function clearList() {
-    list.innerHTML = "";
+    itemsArray.forEach(item => {
+        localStorage.removeItem(`${item.id}`);
+    });
+    document.getElementById('list_habit').innerHTML = "";
     localStorage.removeItem('notes');
     itemsArray = [];
     localStorage.removeItem('notesCount');
-    notesCount = +1;
+    notesCount = 0;
 }
 
-function addContent() {
+let markDone = document.getElementById("mark-done");
+
+function addContent(e) {
+    //console.log(e.target.parentElement)
+    let div_id = e.target.parentNode.id;
+    let week_check = JSON.parse(localStorage.getItem(`${div_id}`));
+    let week_keys = Object.keys(week_check);
+    for (let i = 0; i < week_keys.length; i++) {
+        let day = week_keys[i];
+        let checkbox = document.getElementById(day);
+        if (week_check[`${day}`] == true) {
+            checkbox.type = true;
+            checkbox.style.background = 'red';
+        }
+    }
+    /* 
+        let week_value = [];
+        for (let key in week_check) {
+            week_value += week_check[key];
+        }
+        console.log(week_value)
+        let checkboxes = markDone.querySelectorAll("input");
+        console.log(checkboxes)
+        checkboxes.forEach(item => {
+            item.checked =
+                week_check.
+            console.log(cheeek)
+        }); */
+}
+
+function changeContent(e) {
 
 }
+
+checks = document.querySelectorAll("input[type='checkbox']");
+
+checks.forEach(function (box) {
+    box.addEventListener("change", function (e) {
+        changeContent(e);
+    });
+});
 
 addBtn.addEventListener("click", addStuff);
 clearBtn.addEventListener("click", clearList);
