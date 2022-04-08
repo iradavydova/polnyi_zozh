1ab986e461e3d383bbc3f1fceead1dea997d7340
Maria2721 <84140203+Maria2721@users.noreply.github.com>
2022-04-08 13:24:30 +0300

    add function countChecked()

--- assets/js/habits.js
+++ assets/js/habits.js
@@ -21,6 +21,7 @@ const clearBtn = document.querySelector('.clearBtn');
 const noteInput = document.querySelector('.note');
 let itemsArray = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
 let notesCount = localStorage.getItem('notesCount') ? JSON.parse(localStorage.getItem('notesCount')) : 0;
+let habitCount = localStorage.getItem('habitCount') ? JSON.parse(localStorage.getItem('habitCount')) : 0;
 
 itemsArray.forEach(item => {
     liMaker(item);
@@ -102,10 +103,10 @@ function addContent(e) {
             console.log(checkbox)
         }
     }
+    localStorage.setItem('habitCount', div_id);
 }
 
 function changeContent(e) {
-
     localStorage.removeItem('notes');
     filteredItemsArray = itemsArray.filter((item) => item.id != parent.value);
     localStorage.setItem('notes', JSON.stringify(filteredItemsArray));
@@ -113,15 +114,34 @@ function changeContent(e) {
     itemsArray = filteredItemsArray;
 }
 
-let markDone = document.getElementById("mark-done");
-checks = markDone.querySelectorAll("input[type='checkbox']");
-console.log(checks)
+function countChecked(e) {
+    let checkbox_id = e.target.id;
+    console.log(checkbox_id)
+    let checkbox_checked = e.target.checked;
+    console.log(checkbox_checked)
 
-checks.forEach(function (box) {
-    box.addEventListener("change", function (e) {
-        changeContent(e);
-    });
-});
+    let num_habit = JSON.parse(localStorage.getItem('habitCount'));
+    console.log(num_habit)
+    let week_check = JSON.parse(localStorage.getItem(`${num_habit}`));
+    console.log(week_check.checkbox_id)
+    localStorage.removeItem(`${num_habit}`);
+    for (let i = 0; i < week_keys.length; i++) {
+        let day = week_keys[i];
+        let checkbox = document.getElementById(day);
+        if (week_check[`${day}`] == true) {
+            checkbox.type = true;
+            checkbox.style.background = 'red';
+            console.log(checkbox)
+        }
+    }
+    filteredItemsArray = week_check.filter((item) => item.checkbox_id != parent.value);
+    localStorage.setItem('notes', JSON.stringify(filteredItemsArray));
+    localStorage.removeItem(`${parent.value}`);
+    itemsArray = filteredItemsArray;
+}
+
+let markDone = document.querySelectorAll("input[type='checkbox']");
+markDone.forEach(item => item.addEventListener("click", (e) => countChecked(e)));
 
 addBtn.addEventListener("click", addStuff);
 clearBtn.addEventListener("click", clearList);
