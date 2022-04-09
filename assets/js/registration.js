71cb392a66f7bd5933d889f983c61b03cddaad82
Alena Nemtsova <jelenatbs@gmail.com>
2022-04-09 23:28:45 +0300

    small updates

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,17 +1,23 @@
 let errors = [];
 function checkValidity(input) {
     let validity = input.validity;
-    if (validity.valueMissing) { errors.push('Поле ' + input.placeholder + ' не заполнено'); }
-    if (validity.patternMismatch) { errors.push('Неверный формат заполнения');}
+    if (validity.valueMissing) { errors.push('Пожалуйста, введите ' + input.placeholder); }
+    if (validity.patternMismatch) { errors.push('Введите корректный email-адрес');}
+    if (validity.tooShort) {errors.push('Длина пароля должна быть больше 8 символов')}
 }
 
 function checkAll() {
     errors = [];
     let inputs = document.querySelectorAll("input");
+    let privacy_check = document.getElementById('checkbox1');
 
     for (let input of inputs) {
         checkValidity(input);
     }
+
+    if (privacy_check.checked == false) {errors.push ('Подтвердите принятие условий.')}
     
     document.getElementById('errorsInfo').innerHTML=errors.join('. <br>');
 }
+
+//form.addEventListener('submit', checkAll());
